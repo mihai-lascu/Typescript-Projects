f31f517c977b5551ff1897190b5465e0e06f9921
mihai-lascu <lascu.mihai.teodor@gmail.com>
2022-10-31 22:55:08 +0200

    added project item class

--- Drag&Drop/client/src/app.ts
+++ Drag&Drop/client/src/app.ts
@@ -155,6 +155,28 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 	abstract renderContent(): void;
 }
 
+// ProjectItem Class
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+	private project: Project;
+
+	constructor(hostId: string, project: Project) {
+		super('single-project', hostId, false, project.id);
+		this.project = project;
+
+		this.configure();
+		this.renderContent();
+	}
+
+	configure(): void {}
+
+	renderContent(): void {
+		this.element.querySelector('p')!.textContent = this.project.description;
+		this.element.querySelector('h2')!.textContent = this.project.title;
+		this.element.querySelector('h3')!.textContent =
+			this.project.people.toString();
+	}
+}
+
 // ProjectList Class
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
 	assignedProjects: Project[];
@@ -193,9 +215,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
 		)! as HTMLUListElement;
 		listEl.innerHTML = '';
 		for (const projItem of this.assignedProjects) {
-			const listItem = document.createElement('li');
-			listItem.textContent = projItem.title;
-			listEl?.appendChild(listItem);
+			new ProjectItem(this.element.querySelector('ul')!.id, projItem);
 		}
 	}
 }
--- Drag&Drop/public/app.js
+++ Drag&Drop/public/app.js
@@ -94,6 +94,22 @@ class Component {
         this.hostElement.insertAdjacentElement(insertAtBeginning ? 'afterbegin' : 'beforeend', this.element);
     }
 }
+// ProjectItem Class
+class ProjectItem extends Component {
+    constructor(hostId, project) {
+        super('single-project', hostId, false, project.id);
+        this.project = project;
+        this.configure();
+        this.renderContent();
+    }
+    configure() { }
+    renderContent() {
+        this.element.querySelector('p').textContent = this.project.description;
+        this.element.querySelector('h2').textContent = this.project.title;
+        this.element.querySelector('h3').textContent =
+            this.project.people.toString();
+    }
+}
 // ProjectList Class
 class ProjectList extends Component {
     constructor(type) {
@@ -125,9 +141,7 @@ class ProjectList extends Component {
         const listEl = document.getElementById(`${this.type}-projects-list`);
         listEl.innerHTML = '';
         for (const projItem of this.assignedProjects) {
-            const listItem = document.createElement('li');
-            listItem.textContent = projItem.title;
-            listEl === null || listEl === void 0 ? void 0 : listEl.appendChild(listItem);
+            new ProjectItem(this.element.querySelector('ul').id, projItem);
         }
     }
 }
