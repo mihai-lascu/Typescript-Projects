2280e496bd36e2369243f38e207ad1143acf7d72
mihai-lascu <lascu.mihai.teodor@gmail.com>
2022-11-13 15:58:19 +0200

    added addTodo functionality

--- react-with-ts/src/App.tsx
+++ react-with-ts/src/App.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import NewTodo from './components/NewTodo';
 import TodoList from './components/TodoList';
+import { Todo } from './todo.model';
 
 const App: React.FC = () => {
-	const todos = [{ id: 't1', text: 'Finish the course' }];
+	const [todos, setTodos] = useState<Todo[]>([]);
+
+	const todoAddHandler = (todoText: string) => {
+		setTodos((prevTodos) => [
+			...prevTodos,
+			{ id: Math.random().toString(), text: todoText },
+		]);
+	};
+
 	return (
 		<div className="App">
+			<NewTodo onAddTodo={todoAddHandler} />
 			<TodoList items={todos} />
 		</div>
 	);
--- react-with-ts/src/components/TodoList.tsx
+++ react-with-ts/src/components/TodoList.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Todo } from '../todo.model';
 
 interface TodoListProps {
-	items: { id: string; text: string }[];
+	items: Todo[];
 }
 
 const TodoList: React.FC<TodoListProps> = (props) => {
