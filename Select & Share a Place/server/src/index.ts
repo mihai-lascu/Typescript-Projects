0643aa891d9ecdee53dafba93d9e0c12856e97d7
mihai-lascu <lascu.mihai.teodor@gmail.com>
2022-11-13 13:12:39 +0200

    Added initial setup for Select & Share a Place app

---
+++ Select & Share a Place/server/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const port = 3000;
+const __fileName = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__fileName);
+
+app.use('/', express.static(path.resolve(__dirname, '../../client/public')));
+app.use('/', express.static(path.resolve(__dirname, '../../client/dist')));
+
+app.listen(port, () => {
+	console.log(`Select & Share a Place app listening on port ${port}`);
+});
