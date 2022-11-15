fb81be4d0fa68e1aa38469e2adb168417c8ab9f5
mihai-lascu <lascu.mihai.teodor@gmail.com>
2022-11-15 21:41:28 +0200

    added the last 3 CRUD operations

--- node-with-ts/src/controllers/todos.ts
+++ node-with-ts/src/controllers/todos.ts
@@ -12,3 +12,37 @@ export const createTodo: RequestHandler = (req, res, _next) => {
 
 	res.status(201).json({ message: 'Created the todo.', createdTodo: newTodo });
 };
+
+export const getTodos: RequestHandler = (_req, res, _next) => {
+	res.status(200).json({ todos: TODOS });
+};
+
+export const updateTodo: RequestHandler<{ id: string }> = (req, res, _next) => {
+	const todoId = req.params.id;
+
+	const updatedText = (req.body as { text: string }).text;
+
+	const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+
+	if (todoIndex < 0) {
+		throw new Error('Could not find todo!');
+	}
+
+	TODOS[todoIndex] = new Todo(TODOS[todoIndex].id, updatedText);
+
+	res.status(200).json({ message: 'Updated!', updateTodo: TODOS[todoIndex] });
+};
+
+export const deleteTodo: RequestHandler<{ id: string }> = (req, res, _next) => {
+	const todoId = req.params.id;
+
+	const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+
+	if (todoIndex < 0) {
+		throw new Error('Could not find todo!');
+	}
+
+	TODOS.splice(todoIndex, 1);
+
+	res.status(202).json({ message: 'Todo deleted!' });
+};
